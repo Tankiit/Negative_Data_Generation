fa77875a0d91db704b35885c61815da30ba0ada6
Tanmoy <mukherjee.tanmoy@gmail.com>
2025-04-10 10:23:31 +0200

    Update cifar10_negative.py

--- cifar10_negative.py
+++ cifar10_negative.py
@@ -7,7 +7,7 @@ from openood.networks import ResNet18_32x32
 network = ResNet18_32x32()
 
 # Load the checkpoint
-checkpoint_path = '/Users/cril/tanmoy/research/Out_of_Distribution/Graph_stuff/cifar10_resnet18_32x32_base_e100_lr0.1_default/s0/best.ckpt'
+checkpoint_path = './Out_of_Distribution/Graph_stuff/cifar10_resnet18_32x32_base_e100_lr0.1_default/s0/best.ckpt'
 checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
 network.load_state_dict(checkpoint)
 
